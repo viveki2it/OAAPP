defc3bce6c5bbf22c7536a0c26aa6aa02aa5d493
Vivek <viveki4it@gmail.com>
2019-08-18 19:44:11 +0530

    assets autocomplete

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += Dir["#{Rails.root}/app/assets/javascripts/**/*"].reject {|f| File.directory? f} +
+Dir["#{Rails.root}/app/assets/stylesheets/**/*"].reject {|f| File.directory? f}
