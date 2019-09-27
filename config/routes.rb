b7d4498f89c5faaafa4e40052515682110410153
Vivek <viveki4it@gmail.com>
2019-09-27 08:33:39 +0530

    added sidekiq config

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-
+  mount Sidekiq::Web, at: '/sidekiq', as: 'sidekiq'
   root :to => "users#new"
 
   post 'users/create' => 'users#create'
