db30c50b7310f0030f85f5d7a5c6493bc1b8eacc
Vivek <viveki4it@gmail.com>
2019-10-05 00:18:43 +0530

    added domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,7 +107,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    #:domain         => 'friends.oarsandalps.com',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain,
   }
 end
