22913af844310a4c18149c0949dbfa4297c26c75
Vivek <viveki4it@gmail.com>
2019-11-01 11:04:38 +0530

    added mailgun validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,18 +14,34 @@ class User < ApplicationRecord
   end  
 
   def send_thankyou_email
-    User.all.each do |user|
-      address = ValidEmail2::Address.new(user.email)
-      if address.valid? && address.valid_mx?
-        UserMailer.thank_you_email(user).deliver_later
-        puts "Thankyou eamil sent: #{user.email}"
-      else
-        puts "Failed eamil sent: #{user.email}"
+    User.where("id > 27850").each_slice(1000) do |batch|
+      batch.each do |user|
+        address = ValidEmail2::Address.new(user.email)
+        if address.valid? && address.valid_mx?
+          mailgun_validate = get_validate(user.email)
+          if mailgun_validate.dig('result').eql?('deliverable')
+            UserMailer.thank_you_email(user).deliver_later
+            puts "Thankyou eamil sent: #{user.email}"
+          else
+            puts "Failed mailgun eamil sent: #{user.email}"
+          end
+        else
+          puts "Failed eamil sent: #{user.email}"
+        end
       end
     end
   end
 
   private
+
+  def get_validate(email)
+    url_params = { address: email }
+    public_key = ENV['MAILGUN_API_KEY']
+    validate_url = "https://api.mailgun.net/v4/address/validate"
+    response = RestClient::Request.execute method: :get, url: validate_url, headers: { params: url_params }, user: 'api', password: public_key
+    response_json = JSON.parse response
+    response_json
+  end
 
   def create_referral_code
     self.referral_code = UsersHelper.unused_referral_code
